f63ea8f289b2695da2bef230b268ba2e684c158a
elegardooo <1lagodich@mail.ru>
2022-11-09 22:15:12 +0300

    3 t

--- lb5/3 Task/3 Task.cpp
+++ lb5/3 Task/3 Task.cpp
@@ -31,6 +31,32 @@ void output_array(int** mas, int row, int col)
     }
 }
 
+void array_change(int** mas, int row, int& col)
+{
+    int p, zn;
+    for (int j = 0; j < col; j++)
+    {
+        p = 0;
+        for (int i = 0; i < row; i++)
+        {
+            if (mas[i][j] < 0)
+                p++;
+        }
+        if (p == row)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int c = j; c < col-1; c++)
+                {
+                    mas[i][c] = mas[i][c+1];
+                }
+            }
+            col--;
+            j--;
+        }
+    }
+}
+
 void main(void)
 {
     int **mas, row, col, choose;
@@ -82,6 +108,9 @@ void main(void)
         break;
     }
     printf("Array: ");
+    output_array(mas, row, col);
+    array_change(mas, row, col);
+    printf("\nChanged array: ");
     output_array(mas, row, col);
 }
 
