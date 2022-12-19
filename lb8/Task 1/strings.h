68369d2e7ca21d041ee0b085a5e0916268299c21
elegardooo <1lagodich@mail.ru>
2022-12-19 19:55:42 +0300

    123

--- lb8/Task 1/strings.h
+++ lb8/Task 1/strings.h
@@ -1,1 +1,11 @@
 #pragma once
+#include <stdio.h>
+#include <stdlib.h>
+
+int str_len(char* s);
+char** memory(int argc);
+void input_str(char** str, int argc, char** argv);
+int longest_word_len(char* str);
+void output_str(char** str, int argc);
+void insert_sort(int argc, char** str);
+void freee(char** string, int argc, char** argv);
