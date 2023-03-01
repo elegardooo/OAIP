a78c139ee01f6a9e0b561509af241489018ef264
elegardooo <1lagodich@mail.ru>
2023-03-01 15:12:00 +0300

    123

--- 2sem/lb1/1 Task/1 Task.cpp
+++ 2sem/lb1/1 Task/1 Task.cpp
@@ -57,7 +57,7 @@ void print_structure(computer* laptops, int array_size)
 {
     for (int i = 0; i < array_size; i++)
     {
-        printf("\nName: %s", (laptops)[i].name);
+        printf("\nName: %s", laptops[i].name);
         printf("\nMatrix_frequency: %d", laptops[i].Matrix_frequency);
         printf("\nRAM: %d", laptops[i].RAM);
         printf("\nMemory: %d\n", laptops[i].Memory);
@@ -186,13 +186,119 @@ void free_memory(computer* laptops, int array_size)
     free(laptops);
 }
 
+void deleting(computer* &laptops, int& array_size)
+{
+    int del_num=0;
+    printf("\nChoose laptop number to remove(From 1 to %d): ", array_size);
+    while (scanf_s("%d", &del_num)!= 1 || (del_num < 1) || (del_num > array_size) || getchar()!='\n')
+    {
+        printf("Wrong input. Try again.\n\n");
+        rewind(stdin);
+    }
+    free(laptops[del_num-1].name);
+    for (int i = del_num-1; i < array_size-1; i++)
+    {
+        //laptops[i].name = laptops[i + 1].name;
+        //laptops[i].Matrix_frequency = laptops[i + 1].Matrix_frequency;
+        //laptops[i].RAM = laptops[i + 1].RAM;
+        //laptops[i].Memory = laptops[i + 1].Memory;
+        laptops[i] = laptops[i + 1];
+    }
+    //printf("\n%d", array_size);
+    array_size = array_size - 1;
+   // printf("\n%s\n%s\n%s\n%d\n", laptops[0].name, laptops[1].name, laptops[2].name, array_size);
+    laptops = (computer*)realloc(laptops, array_size  * sizeof(computer));
+    //printf("\n%s\n%s\n", laptops[0].name, laptops[1].name);
+}
+
+int compare_Matrix(computer* laptops, int i)
+{
+    if (laptops[i].Matrix_frequency == laptops[i + 1].Matrix_frequency)
+        return 0;
+    else
+        if (laptops[i].Matrix_frequency > laptops[i + 1].Matrix_frequency)
+            return 1;
+        else
+            return -1;
+}
+
+int compare_RAM(computer* laptops, int i)
+{
+    if (laptops[i].RAM == laptops[i + 1].RAM)
+        return 0;
+    else
+        if (laptops[i].RAM > laptops[i + 1].RAM)
+            return 1;
+        else
+            return -1;
+}
+
+int compare_Memory(computer* laptops, int i)
+{
+    if (laptops[i].Memory == laptops[i + 1].Memory)
+        return 0;
+    else
+        if (laptops[i].Memory > laptops[i + 1].Memory)
+            return 1;
+        else
+            return -1;
+}
+
+void double_sort(computer* laptops, int array_size)
+{
+    int first_sort, second_sort, temp, sort[5];
+    char* tempc;
+    printf("\n1.Sort by Name.\n2.Sort by Matrix Frequency.\n3.Sort by RAM.\n4.Sort by Memory.\nChoose the 1st type of sorting : ");
+    while (scanf_s("%d", &first_sort) != 1 || first_sort < 1 || first_sort >4 || getchar() != '\n')
+    {
+        printf("Wrong input. Try again.\n");
+        rewind(stdin);
+    }
+    switch (first_sort)
+    {
+    case 1:
+        name_sort(laptops, array_size);
+        break;
+    case 2:
+        Matrix_frequency_sort(laptops, array_size);
+        break;
+    case 3:
+        RAM_sort(laptops, array_size);
+        break;
+    case 4:
+        Memory_sort(laptops, array_size);
+        break;
+    }
+    printf("\n1.Sort by Name.\n2.Sort by Matrix Frequency.\n3.Sort by RAM.\n4.Sort by Memory.\nChoose the 2nd type of sorting: ");
+    while (scanf_s("%d", &second_sort) != 1 || second_sort == first_sort || second_sort < 1 || second_sort > 4 || getchar() != '\n')
+    {
+        printf("Wrong input. Try again.\n");
+        rewind(stdin);
+    }
+    for(int i=1; i<array_size; i++)
+        for (int j = 0; j < array_size - 1; j++)
+        {
+            sort[1] = strcmp(laptops[j].name, laptops[j + 1].name);
+            sort[2] = compare_Matrix(laptops, j);
+            sort[3] = compare_RAM(laptops, j);
+            sort[4] = compare_Memory(laptops, j);
+            if (sort[first_sort] == 0 && sort[second_sort] > 0)
+            {
+                tempc = laptops[j].name; laptops[j].name = laptops[j + 1].name; laptops[j + 1].name = tempc;
+                temp = laptops[j].Matrix_frequency; laptops[j].Matrix_frequency = laptops[j + 1].Matrix_frequency; laptops[j + 1].Matrix_frequency = temp;
+                temp = laptops[j].RAM; laptops[j].RAM = laptops[j + 1].RAM; laptops[j + 1].RAM = temp;
+                temp = laptops[j].Memory; laptops[j].Memory = laptops[j + 1].Memory; laptops[j + 1].Memory = temp;
+            }
+        }
+}
+
 void menu(computer* laptops, int array_size)
 {
     int choice = 0, exit = 0;
     //create_structure(laptops, array_size);
     while (exit != 3)
     {
-        printf("-------------------------------------------------\n1.Create structure.\n2.Print structure.\n3.Sort structure.\n4.Exit.\nChoose the option: ");
+        printf("-------------------------------------------------\n1.Create structure.\n2.Print structure.\n3.Sort structure by 1 type.\n4.Sort structure by 2 types.\n5.Remove laptop.\n6.Exit.\nChoose the option: ");
         //input_choice(choice);
         scanf_s("%d", &choice);
         switch (choice)
@@ -223,6 +329,27 @@ void menu(computer* laptops, int array_size)
             sorting(laptops, array_size);
             break;
         case 4:
+            if (exit != 1)
+            {
+                printf("You need to create structure first.\n");
+                break;
+            }
+            double_sort(laptops, array_size);
+            break;
+        case 5:
+            if (exit != 1)
+            {
+                printf("You need to create structure first.\n");
+                break;
+            }
+            if (array_size == 1)
+            {
+                printf("There's only 1 laptop.\n");
+                break;
+            }
+            deleting(laptops, array_size);
+            break;
+        case 6:
             exit = 3;
             break;
         default:
