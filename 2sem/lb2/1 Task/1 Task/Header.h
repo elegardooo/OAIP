8688fa6ee64de58a2cfcf06c9fbde6920821dd5d
elegardooo <1lagodich@mail.ru>
2023-03-29 10:03:10 +0300

    memory + files

--- 2sem/lb2/1 Task/1 Task/Header.h
+++ 2sem/lb2/1 Task/1 Task/Header.h
@@ -1,2 +1,29 @@
 #pragma once
- 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define WORD_MAX_LENGTH 50
+
+typedef struct Words
+{
+    char wordName[WORD_MAX_LENGTH];
+    int count;
+    int size;
+    int lettersnum;
+    int Swapped;
+} Words;
+
+typedef struct Dictionary
+{
+    char DictionaryWord[WORD_MAX_LENGTH];
+    char TranslationWord[WORD_MAX_LENGTH];
+} Dictionary;
+
+int compare(const void* x1, const void* x2);
+int Is_Word(char letter);
+int FindWordIndex(char* name, Words* words, int n);
+void Marks(char* str, int i, FILE* file);
+Words* Words_For_Dictionary(FILE* File_txt, int& WordCount);
+Dictionary* FillDictionary(Words* word, int WordCount, int& DictionaryCount);
+void FileCompressor(FILE* File_txt, Dictionary* dictionary, int DictionaryCount);
+
--- 2sem/lb2/Decompressor/Decompressor/Decompressor/Header.h
+++ 2sem/lb2/Decompressor/Decompressor/Decompressor/Header.h
@@ -1,1 +1,16 @@
 #pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define WORD_MAX_LENGTH 50
+
+
+typedef struct Dictionary
+{
+	char DictionaryWord[WORD_MAX_LENGTH];
+	char TranslationWord[WORD_MAX_LENGTH];
+} Dictionary;
+
+void Marks(char* str, int i, FILE* file);
+Dictionary* Fill_Structure(FILE* Dictionary_txt, int& DictianoryCount);
+void Decompressing(FILE* File_txt, Dictionary* dictionary, int DictionaryCount);
